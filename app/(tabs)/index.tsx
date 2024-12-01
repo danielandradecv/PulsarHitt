e75606c1645ce21439865fcef4b3add6718d0bb0
Daniel Andrade <danielandradecv@gmail.com>
2024-12-01 18:50:34 -0500

    ya bien los banners jeje checkpont

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
       <Text className="text-black text-center text-2xl">PulsarHitt</Text>
 
 
-      <View className="flex-1 md:flex-row md:flex-wrap bg-white items-center justify-center md:space-x-4 md:space-y-4 space-y-4">
+      <View className="flex-1 md:flex-row md:flex-wrap bg-white items-center justify-center md:space-x-4 ">
       <Banner
         imageSource={require('../../assets/images/tabata-timer-banne-tabata.jpg')}
         title="Tabata Timer"
@@ -17,53 +17,18 @@ export default function Index() {
       />
 
 <Banner
-        imageSource={require('../../assets/images/tabata-timer-banne-tabata.jpg')}
+        imageSource={require('../../assets/images/tabata-timer-banner.jpg')}
         title="Tabata Timer"
-        link="/Tab1"
+        link="/Tab2"
+      />
+
+<Banner
+        imageSource={require('../../assets/images/tabata-timer-banne-rest.jpg')}
+        title="Tabata Timer"
+        link="/Tab3"
       />
 
   
-        {/* Primer Banner */}
-        <View className="w-10/12 h-40 rounded-3xl md:w-96 bg-black mb-4 md:mb-0">
-          <ImageBackground
-            source={require('../../assets/images/tabata-timer-banne-tabata.jpg')}
-            className="overflow-hidden"
-            style={{ width: '100%', height: '100%' }}
-            imageStyle={{ borderRadius: 20 }}
-            fadeDuration={700}
-            resizeMode="cover"
-          >
-            <Link href="/Tab1" className="  text-white font-bold text-2xl h-full py-16 w-full text-center">Tabata Timer</Link>
-          </ImageBackground>
-        </View>
-
-        {/* Segundo Banner */}
-        <View className="w-10/12 h-40 rounded-3xl md:w-96 bg-black mb-4 md:mb-0">
-          <ImageBackground
-            source={require('../../assets/images/tabata-timer-banne-tabata.jpg')}
-            className="overflow-hidden"
-            style={{ width: '100%', height: '100%' }}
-            imageStyle={{ borderRadius: 20 }}
-            fadeDuration={700}
-            resizeMode="cover"
-          >
-            <Link href="/Tab1" className="  text-white font-bold text-2xl h-full py-16 w-full text-center">Tabata Timer</Link>
-          </ImageBackground>
-        </View>
-
-        {/* Tercer Banner */}
-        <View className="w-10/12 h-40 rounded-3xl md:w-96 bg-black mb-4 md:mb-0">
-          <ImageBackground
-            source={require('../../assets/images/tabata-timer-banne-tabata.jpg')}
-            className="overflow-hidden"
-            style={{ width: '100%', height: '100%' }}
-            imageStyle={{ borderRadius: 20 }}
-            fadeDuration={700}
-            resizeMode="cover"
-          >
-            <Link href="/Tab1" className="  text-white font-bold text-2xl h-full py-16 w-full text-center">Tabata Timer</Link>
-          </ImageBackground>
-        </View>
         
       </View>
     </View>
